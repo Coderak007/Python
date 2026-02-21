9b4b91a40673adfec3f35f65b5e90e4f05e79dfa
AK <amankumarojha62@gmail.com>
2026-02-21 23:55:56 +0530

    initial commit

--- pythonprogramme10.py
+++ pythonprogramme10.py
@@ -11,14 +11,46 @@ To map with real world scenarios , we startedusing objects in code.
 
 
 # Class is a blueprint for creating objects .
-
+"""
 # creating class
 class Student:
     name = "Aman Ojha"
 
-# creating object(instance)
+# creating object( instance of class)
 s1 = Student()
 print(s1.name)
     
+"""
 
 
+"""
+
+class cars:
+    color = "red"
+    color2 = "blue"
+    brand = "BMW"
+
+s1 = cars()
+s2 = cars()
+s3 = cars()
+print(s1.color)
+print(s2.color2)
+print(s3.brand)    
+
+"""
+
+                                                 # _ _ init _ _ Function
+
+# Construcutor --- all classes have a function called __init__(), which is always executed when the object is bring initiated.
+
+# creating class
+class Student:
+    def __init__(self, fullname):
+        self.name = fullname
+
+# creating onject 
+s1 = Student("Aman")
+print(s1.name) # Aman
+
+s2 = Student("Amit")
+print(s2.name)
