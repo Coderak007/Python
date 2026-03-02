a23d35f850e62a9d1ca0e34169bd1ef437bcf11d
AK <amankumarojha62@gmail.com>
2026-03-02 16:00:15 +0530

    change

--- pythonprogramme11.py
+++ pythonprogramme11.py
@@ -168,6 +168,8 @@ print(Person.name)
 """
 # Property --- We use @property decorator on any method in the class to use the method as a property.
 # eg ---
+"""
+
 class Student:
     def __init__(self, phy, chem, math):
         self.phy = phy
@@ -184,4 +186,82 @@ print(stu1.percentage)
 stu1.phy = 100
 print(stu1.percentage)
 
+"""
+                                         # Polymorphism : Operator Overloading
 
+# When the same operator is allowed to have different meaning according to the context.( Dunder functions )
+
+"""
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real,"i + ",self.img,"j")
+
+    def __add__(self, num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal , newImg)
+    def __sub__(self, num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img
+        return Complex(newReal , newImg)
+
+num1 = Complex(1, 5)
+num1.showNumber()
+
+num2 = Complex(4, 6)
+num2.showNumber()
+
+num3 = num1 - num2
+num3.showNumber()
+   
+"""
+
+# Let's Practice ---
+# Define a circle class to create with radius r using the constructor. define an area() method of the class which calculates the area of the circle. define a perimeter() method of the class which allows you to calculate the perimeter of the circle.
+
+"""
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 22/7 * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * 22/7 * self.radius
+    
+        
+c1 = Circle(21)
+print(c1.area())
+print(c1.perimeter())
+
+"""
+
+# Define a Employee class with attribute role, department & salary . this class also showDetails() method. Create an Engineer class that inherits properties from employee & has additional attributes : name & age
+
+
+class Employee:
+    def __init__(self, role, dept, salary):
+        self.role = role
+        self.dept = dept
+        self.salary = salary
+
+    def showDetails(self):
+        print("role = ", self.role)
+        print("dept = ", self.dept) 
+        print("salary = ",self.salary)
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age 
+        super().__init__("Engineer", "IT","75,000")
+       
+
+
+engg1 = Engineer("Aman" , 22)
+engg1.showDetails()           
